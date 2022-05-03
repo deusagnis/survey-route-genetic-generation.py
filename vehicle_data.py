53a4bfe1aaef8e9153461ba6c918f1b65a070249
Deusagnis <deusagnis@gmail.com>
2022-05-04 02:03:20 +0300

    Correct vehicle data necessary attributes.

--- vehicle_data.py
+++ vehicle_data.py
@@ -1,6 +1,10 @@
 class VehicleData:
-    def __init__(self, turn_rate, average_speed, flight_altitude, vision_width):
+    def __init__(self, turn_rate, average_speed, vision_width):
+        """
+        :param turn_rate: На сколько градусов БПЛА может повернуть на 1 скорости в м/с
+        :param average_speed: Средняя предполагаемая скорость БПЛА (Крейсерская?)
+        :param vision_width: Ширина приборного "зрения"
+        """
         self.turn_rate = turn_rate
         self.average_speed = average_speed
-        self.flight_altitude = flight_altitude
         self.vision_width = vision_width
