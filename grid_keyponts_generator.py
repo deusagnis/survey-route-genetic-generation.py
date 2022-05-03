94964320df8573051858396a650d03baefa45e69
Deusagnis <deusagnis@gmail.com>
2022-05-04 01:59:45 +0300

    Create grid keypoints generator.

---
+++ grid_keyponts_generator.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class GridKeypointsGenerator:
+    def __init__(self, borders, lat_step, lon_step):
+        self.borders = borders
+        self.lon_step = lon_step
+        self.lat_step = lat_step
+
+    def _gen_grid(self):
+        grid = []
+        for lat in np.arange(self.borders["lat_bot"], self.borders["lat_top"] + self.lat_step, self.lat_step):
+            for lon in np.arange(self.borders["lon_left"], self.borders["lon_right"] + self.lon_step, self.lon_step):
+                grid.append([lat, lon])
+
+        return grid
+
+    def gen(self):
+        # двойным циклом с шагами создаём ключевые точки
+        return self._gen_grid()
