e47f239f5af7997e37ca64cefbdd2e4242f0fe22
Deusagnis <deusagnis@gmail.com>
2022-05-06 21:11:47 +0300

    Update route generator factory.

--- survey_route_generation/factories/route_generator_factory.py
+++ survey_route_generation/factories/route_generator_factory.py
@@ -5,27 +5,18 @@ from survey_route_generation.route_generator import RouteGenerator
 
 class RouteGeneratorFactory:
     def __init__(self,
-                 vehicle_vision_width=None,
-                 mission_start_point=None,
-                 mission_end_point=None,
-                 survey_area_points=None,
                  population_size=None,
                  selection_rate=None,
                  parents_count=None,
                  mutants_rate=None,
                  max_lifecycles=None,
                  parents_choice_type=None,
+                 parents_similarity_type=None,
                  mutation_swap_value=None,
                  mutation_swap_type=None,
                  route_distance_weight=None,
                  route_turns_angle_weight=None
                  ):
-        self.vehicle_vision_width = vehicle_vision_width
-
-        self.mission_start_point = mission_start_point
-        self.mission_end_point = mission_end_point
-
-        self.survey_area_points = survey_area_points
 
         self.population_size = population_size,
         self.selection_rate = selection_rate,
@@ -33,6 +24,7 @@ class RouteGeneratorFactory:
         self.mutants_rate = mutants_rate,
         self.max_lifecycles = max_lifecycles,
         self.parents_choice_type = parents_choice_type
+        self.parents_similarity_type = parents_similarity_type
 
         self.mutation_swap_value = mutation_swap_value
         self.mutation_swap_type = mutation_swap_type
@@ -47,6 +39,7 @@ class RouteGeneratorFactory:
             self.mutants_rate,
             self.max_lifecycles,
             self.parents_choice_type,
+            self.parents_similarity_type
         )
 
         genetic_optimal_route_finder = GeneticOptimalRouteFinder(
