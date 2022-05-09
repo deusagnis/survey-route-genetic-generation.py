993fd081a6e2efb90e9ff70174cc801abb61108c
Deusagnis <deusagnis@gmail.com>
2022-05-10 00:45:46 +0300

    Add necessary dirs creation to setup script.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import setuptools
+from env import ROOT_DIR
+from os import mkdir
 
 with open('requirements.txt', 'r') as f:
     install_requires = f.read().splitlines()
@@ -6,3 +8,8 @@ with open('requirements.txt', 'r') as f:
 setuptools.setup(name='survey_route_generation',
                  packages=['survey_route_generation'],
                  install_requires=install_requires)
+
+mkdir(ROOT_DIR + "\\output")
+mkdir(ROOT_DIR + "\\output\\data")
+mkdir(ROOT_DIR + "\\output\\logs")
+mkdir(ROOT_DIR + "\\output\\results")
